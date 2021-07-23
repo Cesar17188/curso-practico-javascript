a4270129931a7070c64f9cb8e4a13a67ce006648
cesareli17188 <cesareli17188@gmail.com>
2021-07-22 20:47:58 -0500

    moda inicializado

--- estadistica/moda.js
+++ estadistica/moda.js
@@ -1,1 +1,24 @@
 const lista1 = [1, 2, 3, 1, 2, 3, 4, 2, 2, 2, 1];
+
+function calcularModa(lista) {
+  const listaCount = {};
+
+  lista.map(function (elemento) {
+    if (listaCount[elemento]) {
+      listaCount[elemento] += 1;
+    } else {
+      listaCount[elemento] = 1;
+    }
+  });
+
+  const listaArray = Object.entries(listaCount).sort(function (
+    elementoA,
+    elementoB
+  ) {
+    return elementoA[1] - elementoB[1];
+  });
+
+  const moda = listaArray[listaArray.length - 1];
+
+  return moda;
+}
